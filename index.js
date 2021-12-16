10e43d17e5ae4bdfecc16c5480127854b5255ac0
SE <95391540+riiix3@users.noreply.github.com>
2021-12-16 04:29:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get('/fasttext/', function(req, res) {
 
 function getFastTextResults(statement) {
 	//predict returns an array with the input and predictions for best cateogires
+  var result = null;
 	FastText.predict(
 		"model.bin", 3,
 		[statement],
@@ -42,9 +43,9 @@ function getFastTextResults(statement) {
 			console.log(error)
 			return;
 		  }
-		  console.log(success)
+		  result=success;
 		})
-	return "success!";
+	return result;
 }
 
 app.listen(8000, () => {
